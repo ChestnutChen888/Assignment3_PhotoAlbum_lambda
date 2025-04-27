49364b1947b5a4f05eff28192f384d46a9f4c2a4
Zhenzhen Chen <czz0917@gmail.com>
2025-04-26 20:58:40 -0400

    Add appspec.yml and scripts for CodeDeploy

---
+++ scripts/update_lambda_index.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Updating index-photos Lambda function (fake update for CodeDeploy check)"
---
+++ scripts/update_lambda_search.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Updating search-photos Lambda function (fake update for CodeDeploy check)"
